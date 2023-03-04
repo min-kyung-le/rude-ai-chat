e1490c27c8e84bb8b89d404c725aa7aa4713a3e9
mink <minkyung1435@gmail.com>
2023-03-04 22:54:22 +0900

    first commit

---
+++ frontend/src/pages/Movies.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>영화 목록</h1>
+    <div v-for="movie in movies" :key="movie.id">
+      <img v-bind:src="movie.poster" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  mounted() {
+    this.getMovies();
+  },
+  data() {
+    return {
+      movies: [
+        {
+          id: Number,
+          name: String,
+          year: Number,
+          director: String,
+          poster: String,
+        },
+      ],
+    };
+  },
+  methods: {
+    getMovies(): object {
+      return this.$http.get("/api/movies").then((response) => {
+        this.movies = response.data;
+      });
+    },
+  },
+});
+</script>
+
+<style lang="" scoped></style>
---
+++ frontend/types/vue-global.d.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $http: typeof axios;
+  }
+}
