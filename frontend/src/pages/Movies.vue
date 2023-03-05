923d76f58b21cc8e3f668438c3e11b042d446da5
mink <minkyung1435@gmail.com>
2023-03-05 12:46:43 +0900

    영화 목록 상세 정보 뜨도록 수정

--- frontend/src/pages/Movies.vue
+++ frontend/src/pages/Movies.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h1>영화 목록</h1>
-    <div v-for="movie in movies" :key="movie.id">
-      <img v-bind:src="movie.poster" />
+    <div v-for="movie in movies" :key="movie.id" class="movies">
+      <img
+        :src="movie.poster"
+        :alt="movie.name"
+        @click="movieInfo(Number(movie.id))"
+      />
     </div>
+    <h1>상세 정보</h1>
+    <div>제목 : {{ moviesInfo.name }}</div>
+    <div>년도 : {{ moviesInfo.year }}</div>
+    <div>배우 : {{ moviesInfo.director }}</div>
   </div>
 </template>
 
@@ -12,10 +20,18 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   mounted() {
+    this.movieInfo(1);
     this.getMovies();
   },
   data() {
     return {
+      moviesInfo: {
+        id: Number,
+        name: String,
+        year: Number,
+        director: String,
+        poster: String,
+      },
       movies: [
         {
           id: Number,
@@ -28,6 +44,11 @@ export default defineComponent({
     };
   },
   methods: {
+    movieInfo(id: number): object {
+      return this.$http.get("/api/movies/" + id).then((response) => {
+        this.moviesInfo = response.data[0];
+      });
+    },
     getMovies(): object {
       return this.$http.get("/api/movies").then((response) => {
         this.movies = response.data;
@@ -37,4 +58,13 @@ export default defineComponent({
 });
 </script>
 
-<style lang="" scoped></style>
+<style scoped>
+.movies {
+  display: inline-block;
+  margin: 0 25px 0 0;
+}
+.movies img {
+  width: 210px;
+  height: 300px;
+}
+</style>
