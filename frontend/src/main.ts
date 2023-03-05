b6e9c8f34f96b3634241a0ab8fbc02fe74abad0e
mink <minkyung1435@gmail.com>
2023-03-05 17:12:44 +0900

    vuetify 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,22 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 import router from "./router";
 import axios from "axios";
 
-// Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+loadFonts();
 
 const app = createApp(App);
-
 app.use(router).use(vuetify).mount("#app");
 
 app.config.globalProperties.$http = axios;
