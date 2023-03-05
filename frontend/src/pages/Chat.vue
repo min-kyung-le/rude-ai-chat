4e204fcc586e608ea4b58fc8e8b7ada3d331b499
mink <minkyung1435@gmail.com>
2023-03-05 17:26:22 +0900

    Vuetfiy 무료 테마로 화면 구성

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -1,18 +1,62 @@
 <template>
-  <div>
-    <h1><v-icon icon="mdi-home" />4가지 없는 AI 채팅</h1>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
+
+        <v-btn v-for="link in links" :key="link" variant="text">
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-title> List Item {{ n }} </v-list-item-title>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item link color="grey-lighten-4">
+                  <v-list-item-title> Refresh </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Chat",
-  setup(props, { emit, attrs, expose, slots }) {
-    return {};
-  },
+  data: () => ({
+    links: ["예의바른 AI Chat봇", "4가지 없는 AI Chat봇"],
+  }),
 });
 </script>
-
-<style scoped lang="scss"></style>
+<style scoped></style>
