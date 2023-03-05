17ade7a3108e2a786030de4e18292f11b9a5cb45
mink <minkyung1435@gmail.com>
2023-03-05 17:08:52 +0900

    chat 화면 구성 중

---
+++ frontend/src/pages/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1><v-icon icon="mdi-home" />4가지 없는 AI 채팅</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Chat",
+  setup(props, { emit, attrs, expose, slots }) {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,8 +3,8 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
   {
     path: "/",
-    name: "Movies",
-    component: () => import("./pages/Movies.vue"),
+    name: "Chat",
+    component: () => import("./pages/Chat.vue"),
   },
 ];
 
