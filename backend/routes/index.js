92c21c887376c9e08d1fb7e4fdfaf4c8a278fbcd
mink <minkyung1435@gmail.com>
2023-03-06 21:32:45 +0900

    Node의 router와 Vue의 router 연동

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,11 @@ var express = require("express");
 var router = express.Router();
 var generate = require("../api/generate");
 
-router.post("/", async function (req, res) {
+router.post("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+router.post("/api/generate", async function (req, res) {
   const answer = await generate(req, res);
   res.send(answer);
 });
