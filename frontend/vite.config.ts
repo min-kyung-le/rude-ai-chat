c965a629e565cf248fcd54b1ff6c54de6d71b1c9
mink <minkyung1435@gmail.com>
2023-03-06 21:29:35 +0900

    frontend빌드 경로 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,9 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
+  build: {
+    outDir: path.resolve("../backend/public"),
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
